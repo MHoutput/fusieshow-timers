0a5ed9a2507c432040c312be2e4b540931f45e86
MHoutput <67870185+MHoutput@users.noreply.github.com>
2023-06-01 18:06:30 +0200

    v2 Background images
    Updated version of the code where the countdown timers may now have a background image. Also added support to directly export the timer as an MP4 video, rather than a GIF which has to be converted externally.

--- Countdowns_Fusieshow.m
+++ Countdowns_Fusieshow.m
@@ -1,25 +1,20 @@
-%% Toen de timers gemaakt werden, 2023-02-21:
+%% Make timers for the Fusieshow, with a background image
 
-%Aantal olievaten:
-makeCountdown(1411724488674, -1123.887395833333, 180, 'Olievaten')
+%Amount of oil remaining, in barrels:
+makeCountdown(1385409339633, -1123.887395833333, 180, 'OilReserves','oilbarrels.png')
+%The data from this timer comes from
+%https://www.worldometers.info/oil
+%Roughly 1123 barrels of oil are used every second, 1385409339633 is the
+%projected oil reserves on 2023-11-19
 
-%Wereldbevolking:
-makeCountdown(8019096947, 2.262212688581683, 180, 'Wereldbevolking')
+%World population:
+makeCountdown(8072065300, 2.262212688581683, 180, 'WorldPopulation','earth.png')
+%The data for this timer comes from
+%https://www.theworldcounts.com/populations/world/people
+%The net growth is 2.26 people per second, 8072065300 is the projected
+%world population on 2023-11-19
 
+%% Make timers without background image
 
-%% Op de derde dag van Nerdland, 2023-05-28:
-
-%Aantal olievaten:
-makeCountdown(1402402517058, -1123.887395833333, 180, 'Olievaten_Nerdland')
-
-%Wereldbevolking:
-makeCountdown(8037860644, 2.262212688581683, 180, 'Wereldbevolking_Nerdland')
-
-
-%% Op de derde dag van de Fusieshow, 2023-11-19:
-
-%Aantal olievaten:
-makeCountdown(1385409339633, -1123.887395833333, 180, 'Olievaten_Fusieshow')
-
-%Wereldbevolking:
-makeCountdown(8072065300, 2.262212688581683, 180, 'Wereldbevolking_Fusieshow')
+makeCountdown(1385409339633, -1123.887395833333, 180, 'OilReserves_plain')
+makeCountdown(8072065300, 2.262212688581683, 180, 'WorldPopulation_plain')
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # fusieshow-timers
 
-This simple Matlab code creates GIF countdown timers for the world population and the oil reserves, for use at the start of the Fusieshow presentation.
+This Matlab code creates GIF countdown timers for the world population and the oil reserves, for use at the start of the Fusieshow presentation.
 makeCountdown.m creates a general timer, and Countdowns_Fusieshow.m contains some examples to create the timers specifically for the world population and the oil reserves on the show dates in 2023.
-
-If you convert the GIF to mp4 externally (for example at https://cloudconvert.com/gif-to-mp4), the file size decreases significantly.
